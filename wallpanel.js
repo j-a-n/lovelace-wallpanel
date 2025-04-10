407867515fce0c1ffc4394dfdf468be12c11b0dc
Sven Wagner <svenw@posteo.de>
2025-04-10 12:11:23 +0200

    add immich people support

--- wallpanel.js
+++ wallpanel.js
@@ -46,6 +46,7 @@ const defaultConfig = {
 	immich_album_names: [],
 	immich_shared_albums: true,
 	immich_tag_names: [],
+	immich_persons: [],
 	immich_resolution: "preview",
 	image_fit: "cover", // cover / contain / fill
 	image_list_update_interval: 3600,
@@ -2273,7 +2274,54 @@ function initWallpanel() {
 
 			const http = new XMLHttpRequest();
 			http.responseType = "json";
-			if (config.immich_tag_names && config.immich_tag_names.length) {
+			if (config.immich_persons && config.immich_persons.length) {
+				const personNames = config.immich_persons.map((v) => v.toLowerCase());
+				http.open("GET", `${apiUrl}/people`, true);
+				http.setRequestHeader("x-api-key", config.immich_api_key);
+				http.onload = function () {
+					const personIds = [];
+					if (http.status == 200 || http.status === 0) {
+						const allPersons = http.response;
+						logger.debug(`Got immich API response`, allPersons);
+						allPersons.people.forEach((person) => {
+							logger.debug(person);
+							if (!personNames.includes(person.name.toLowerCase())) {
+								logger.debug("Skipping person: ", person.name);
+							} else {
+								logger.debug("Adding person: ", person.name);
+								personIds.push(person.id);
+							}
+						});
+						if (personIds.length > 0) {
+							const http2 = new XMLHttpRequest();
+							http2.responseType = "json";
+							http2.open("POST", `${apiUrl}/search/metadata`, true);
+							http2.setRequestHeader("x-api-key", config.immich_api_key);
+							http2.setRequestHeader("Content-Type", "application/json");
+							logger.debug("Searching asset metdata for person: ", personIds);
+							http2.onload = function () {
+								if (http2.status == 200 || http2.status === 0) {
+									const searchResults = http2.response;
+									logger.debug(`Got immich API response`, searchResults);
+									processAssets(searchResults.assets.items);
+									processUrls();
+								} else {
+									logger.error("Immich API error", http2);
+								}
+							};
+							http2.send(JSON.stringify({ personIds: personIds, withExif: true, size: 1000 }));
+						} else {
+							logger.error("No immich person selected");
+							wp.updatingImageList = false;
+						}
+					} else {
+						logger.error("Immich API error", http);
+						wp.updatingImageList = false;
+					}
+				};
+				http.send({});
+			}
+			else if (config.immich_tag_names && config.immich_tag_names.length) {
 				const tagNames = config.immich_tag_names.map((v) => v.toLowerCase());
 				http.open("GET", `${apiUrl}/tags`, true);
 				http.setRequestHeader("x-api-key", config.immich_api_key);
